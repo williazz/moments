e7793f10255ee137b6881618ca9cbc1e05e4f408
William Z <williazz@usc.edu>
2022-03-04 13:25:51 -0800

    used open_mail_app package to route email link to ios email apps

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:moments/firebase_options.dart';
+import 'package:open_mail_app/open_mail_app.dart';
 
 class MomentsApp extends StatelessWidget {
   const MomentsApp({Key? key}) : super(key: key);
@@ -49,10 +50,50 @@ class LoginPage extends StatelessWidget {
             children: [
               EmailLinkSignInView(
                   config: DefaultFirebaseOptions.emailLinkProviderConfig),
+              ElevatedButton(
+                  onPressed: () async {
+                    final result = await OpenMailApp.openMailApp();
+                    if (!result.didOpen && !result.canOpen) {
+                      showNoMailAppsDialog(context);
+                    } else if (!result.didOpen && result.canOpen) {
+                      showDialog(
+                        context: context,
+                        builder: (_) {
+                          return MailAppPickerDialog(
+                            mailApps: result.options,
+                          );
+                        },
+                      );
+                    }
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Open Mail App'),
+                  )),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  showNoMailAppsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Open Mail App"),
+          content: const Text("No mail apps installed"),
+          actions: <Widget>[
+            OutlinedButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      },
     );
   }
 }
