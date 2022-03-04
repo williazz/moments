90929ddabe6c4402d3224d64cdbb6d8950682af0
William Z <williazz@usc.edu>
2022-03-04 00:16:39 -0800

    email link sign in

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart' show ProfileScreen, SignInScreen;
 
 class MomentsApp extends StatelessWidget {
   const MomentsApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Moments',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Moments Home')),
-      ),
+      home: AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const SignInScreen();
+        }
+        return const ProfileScreen();
+      },
     );
   }
 }
