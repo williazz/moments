8dd27b84883b2398355ebceafa07ab48f224ab9a
William Z <williazz@usc.edu>
2022-03-03 21:02:17 -0800

    firebase auth login success

--- lib/services/setup.dart
+++ lib/services/setup.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:moments/firebase_options.dart';
@@ -7,4 +8,11 @@ setup() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  try {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: 'williamz.zhou1@gmail.com', password: 'password');
+    print('success');
+  } catch (e) {
+    print(e);
+  }
 }
