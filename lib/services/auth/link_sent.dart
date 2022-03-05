646852cea918c133dffcfdcc60ad5e29a0f06d93
William Z <williazz@usc.edu>
2022-03-04 17:35:26 -0800

    Unpolished UI implements UX logic that opens list of mail apps

--- lib/services/auth/link_sent.dart
+++ lib/services/auth/link_sent.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:open_mail_app/open_mail_app.dart';
 
-class LinkSentPage extends StatelessWidget {
+class LinkSentPage extends StatefulWidget {
   final String email;
   const LinkSentPage({
     Key? key,
@@ -9,9 +10,22 @@ class LinkSentPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<LinkSentPage> createState() => _LinkSentPageState();
+}
+
+class _LinkSentPageState extends State<LinkSentPage> {
+  @override
+  void initState() {
+    super.initState();
+    _openEmailApp(context);
+  }
+
+  @override
   Widget build(BuildContext context) {
     const gap = 10.0;
-
+    final textTheme = Theme.of(context).textTheme;
+    // TODO: change to use provided theme onPrimary
+    // final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(),
@@ -21,18 +35,17 @@ class LinkSentPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Text('Check your email',
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6!
-                    .copyWith(fontWeight: FontWeight.bold)),
+                style: Theme.of(context).textTheme.headline5!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    )),
             const SizedBox(height: gap),
             RichText(
                 text: TextSpan(
                     text: 'We sent a magic login link to: ',
-                    style: Theme.of(context).textTheme.subtitle1,
+                    style: textTheme.subtitle1,
                     children: [
                   TextSpan(
-                      text: email,
+                      text: widget.email,
                       style: Theme.of(context).textTheme.subtitle1!.copyWith(
                             color: Colors.blue,
                           )),
@@ -43,7 +56,7 @@ class LinkSentPage extends StatelessWidget {
                 style: Theme.of(context).textTheme.subtitle1),
             Expanded(child: Container()),
             OutlinedButton.icon(
-                onPressed: () {},
+                onPressed: () => _openEmailApp(context),
                 icon: const Icon(CupertinoIcons.mail),
                 label: const Padding(
                   padding: EdgeInsets.all(gap * 2),
@@ -53,6 +66,45 @@ class LinkSentPage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  _openEmailApp(BuildContext context) async {
+    var result = await OpenMailApp.openMailApp();
+
+    // If no mail apps found, show error
+    if (!result.didOpen && !result.canOpen) {
+      _showNoMailAppsDialog(context);
+    } else if (!result.didOpen && result.canOpen) {
+      showDialog(
+        context: context,
+        builder: (_) {
+          return MailAppPickerDialog(
+            title: 'Open Mail',
+            mailApps: result.options,
+          );
+        },
+      );
+    }
+  }
+
+  void _showNoMailAppsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Open Mail App"),
+          content: const Text("No mail apps installed"),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      },
     );
   }
 }
