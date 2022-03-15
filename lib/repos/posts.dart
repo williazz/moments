a5eb32b842d381ac0b696aad7281045f09d7fcb0
William Z <williazz@usc.edu>
2022-03-15 15:40:47 -0400

    create naive implementation of PostsRepo

--- lib/repos/posts.dart
+++ lib/repos/posts.dart
@@ -1,5 +1,55 @@
-abstract class PostsConfig {
-  static const key = 'posts';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+@immutable
+class Post {
+  final String title, body;
+  const Post({
+    required this.title,
+    required this.body,
+  });
+
+  Post.fromJson(Map<String, dynamic> json)
+      : this(title: json['title']!, body: json['body']!);
+
+  Map<String, dynamic> toJson() {
+    return {'title': title, 'body': body};
+  }
 }
 
-abstract class PostsRepo {}
+abstract class PostsRepo {
+  static const collectionKey = 'posts';
+  Future<Post?> getById(String docId);
+  Future<void> add(Post post);
+  Future<List<Post>> getAll();
+}
+
+class FirebasePostsRepo extends PostsRepo {
+  late final CollectionReference<Post> _collection;
+  FirebasePostsRepo() {
+    _collection = FirebaseFirestore.instance
+        .collection(PostsRepo.collectionKey)
+        .withConverter<Post>(
+            fromFirestore: (snapshot, _) => Post.fromJson(snapshot.data()!),
+            toFirestore: (post, _) => post.toJson());
+  }
+
+  @override
+  getById(String docId) async {
+    final snapshot = await _collection.doc(docId).get();
+    return snapshot.data();
+  }
+
+  @override
+  add(Post post) async {
+    await _collection.add(post);
+  }
+
+  @override
+  getAll() async {
+    final querySnapshot = await _collection.limit(10).get();
+    final posts =
+        querySnapshot.docs.map((snapshot) => snapshot.data()).toList();
+    return posts;
+  }
+}
