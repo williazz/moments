43bc066f7f7988ceb4487141cebb0a74bd0f2766
William Z <williazz@usc.edu>
2022-03-23 15:07:40 -0400

    added isRegistered() method

--- lib/repos/users.dart
+++ lib/repos/users.dart
@@ -2,14 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
 @immutable
-class UserDetails {
+class UserModel {
   final String username, uuid;
-  const UserDetails({
+  const UserModel({
     required this.uuid,
     required this.username,
   });
 
-  UserDetails.fromJson(Map<String, dynamic> json)
+  UserModel.fromJson(Map<String, dynamic> json)
       : this(uuid: json['uuid'], username: json['username']);
 
   Map<String, dynamic> toJson() => {'uuid': uuid, 'username': username};
@@ -17,24 +17,28 @@ class UserDetails {
 
 abstract class UsersRepo {
   static const collectionKey = 'user-details';
-  Future<UserDetails?> getByUUID(String uuid);
-  Future<void> create(UserDetails user);
+  Future<UserModel?> getByUUID(String uuid);
+  Future<void> create(UserModel user);
+  Future<bool> isRegistered(String uuid) async {
+    final user = await getByUUID(uuid);
+    return user != null;
+  }
 }
 
 class FirestoreUsersRepo extends UsersRepo {
-  late final CollectionReference<UserDetails> _collection;
+  late final CollectionReference<UserModel> _collection;
 
   FirestoreUsersRepo() {
     _collection = FirebaseFirestore.instance
         .collection(UsersRepo.collectionKey)
-        .withConverter<UserDetails>(
+        .withConverter<UserModel>(
             fromFirestore: (snapshot, _) =>
-                UserDetails.fromJson(snapshot.data()!),
-            toFirestore: (userDetails, _) => userDetails.toJson());
+                UserModel.fromJson(snapshot.data()!),
+            toFirestore: (user, _) => user.toJson());
   }
 
   @override
-  create(UserDetails user) async {
+  create(UserModel user) async {
     await _collection.add(user);
   }
 
