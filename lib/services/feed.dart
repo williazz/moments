94320ec80aab5c2b230bf381cdadb31f3359d041
William Z <williazz@usc.edu>
2022-03-25 17:13:17 -0400

    FeedService gets profile feed

--- lib/services/feed.dart
+++ lib/services/feed.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 
+import 'package:dcache/dcache.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:moments/repos/posts.dart';
@@ -7,13 +8,17 @@ import 'package:moments/repos/posts.dart';
 abstract class FeedService extends ChangeNotifier {
   Future<List<Post>> getAll();
   Future<void> add(Post post);
-
   List<Post> _posts = [];
   List<Post> get posts => UnmodifiableListView(_posts);
+  Future<List<Post>> getSnapshotByProfile(String username,
+      {bool update = false});
 }
 
 class FirestoreFeedService extends FeedService {
   final _collection = GetIt.I<PostsRepo>();
+  final _cache = SimpleCache<String, List<Post>>(
+      storage: InMemoryStorage<String, List<Post>>(20));
+
   @override
   Future<List<Post>> getAll() async {
     notifyListeners();
@@ -28,5 +33,16 @@ class FirestoreFeedService extends FeedService {
     _posts.insert(0, post);
     notifyListeners();
     return post;
+  }
+
+  @override
+  getSnapshotByProfile(String username, {bool update = false}) async {
+    if (!update && _cache.containsKey(username)) {
+      return _cache.get(username)!;
+    }
+    final feed = await _collection.getAllByUsername(username);
+    _cache.set(username, feed);
+    notifyListeners();
+    return feed;
   }
 }
